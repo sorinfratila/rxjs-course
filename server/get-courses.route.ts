81f45f6a5ed74d57ad7c973cd450f4765eebd826
Sorin Fratila <sorin.fratila@civicplus.com>
2023-03-28 17:37:47 +0200

    removed clutter

--- server/get-courses.route.ts
+++ server/get-courses.route.ts
@@ -7,22 +7,19 @@ import {COURSES} from "./db-data";
 
 export function getAllCourses(req: Request, res: Response) {
 
-/*
     const error = (Math.random() >= 0.5);
 
     if (error) {
         console.log("ERROR loading courses!");
         res.status(500).json({message: 'random error occurred.'});
     }
-    else { */
-
+    else {
         setTimeout(() => {
 
-             res.status(200).json({payload:Object.values(COURSES)});
+            res.status(200).json({payload: Object.values(COURSES)});
 
         }, 200);
-
-  //  }
+    }
 }
 
 
@@ -30,7 +27,7 @@ export function getCourseById(req: Request, res: Response) {
 
     const courseId = req.params["id"];
 
-    const courses:any = Object.values(COURSES);
+    const courses: any = Object.values(COURSES);
 
     const course = courses.find(course => course.id == courseId);
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {CourseComponent} from "./course/course.component";
 
 const routes: Routes = [
     {
@@ -10,22 +8,10 @@ const routes: Routes = [
         component: HomeComponent
 
     },
-    {
-        path: "about",
-        component: AboutComponent
-    },
-    {
-        path: 'courses/:id',
-        component: CourseComponent
-    },
-    {
-        path: "**",
-        redirectTo: '/'
-    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
